85c6cead93db2468e648fdace2d7c20d0fd3c4f0
patchn <71460349+patchn@users.noreply.github.com>
2022-05-23 07:14:07 -0400

    wasd

--- 11ty/.eleventy.js
+++ 11ty/.eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/style")
     eleventyConfig.addPassthroughCopy("src/md")
     eleventyConfig.addPassthroughCopy("src/media")
+    eleventyConfig.addPassthroughCopy("src/admin")
     eleventyConfig.addFilter("postDate", (dateObj) =>{
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
